5f15076b38f2140db80f3d0a10e1b09b14cf1af4
sommeow <s.pimson@gmail.com>
2019-07-15 22:34:44 -0500

    push before deployment

--- mern-auth/server.js
+++ mern-auth/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = required('path');
 const passport = require("passport");
 const app = express();
 const users = require("./routes/api/users");
@@ -30,6 +31,16 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+// set a static folder
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+}
 
 const port = process.env.PORT || 5000;
 
